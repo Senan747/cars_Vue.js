d853f07e05e660567e8be4db4c4289d7451cb65c
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-03-12 22:25:04 +0400

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="container">
+        <h1>ABOUT</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, reprehenderit laudantium nostrum veritatis adipisci doloribus illum, pariatur aut aliquid neque dolore dolores, voluptates facere. Excepturi ducimus nesciunt dolorum voluptas! Aperiam?</p>
+    </main>
+   
+</template>
+
+<style scoped>
+  p{
+    text-align: center;
+  }
+</style>
---
+++ src/views/CarView.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {useRoute} from 'vue-router'
+  import {ref, onBeforeMount} from "vue"
+  import cars from "../data.json"
+  const route = useRoute()
+
+  onBeforeMount(() => {
+    const carData = cars.find(c => c.id === parseInt(id))
+  })
+  const {id} = route.params
+</script>
+<template>
+    <div class="container">
+        <h1>The car</h1>
+    </div>
+</template>
